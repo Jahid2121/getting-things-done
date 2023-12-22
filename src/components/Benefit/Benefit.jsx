5181bfae273954bbf73881a969b69b856eff7065
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-12-22 19:31:02 +0600

    Update website name

--- src/components/Benefit/Benefit.jsx
+++ src/components/Benefit/Benefit.jsx
@@ -7,11 +7,11 @@ const Benefit = () => {
         <h2 className="text-3xl font-bold mb-4">Who Can Benefit?</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {benefits.map((benefit) => (
-            <div key={benefit.type} className="bg-gray-200 p-4 rounded-md">
+            <div key={benefit._id} className="bg-gray-200 p-4 rounded-md">
               <h3 className="text-xl font-semibold mb-2">{benefit.type}</h3>
               <ul>
-                {benefit.benefits.map((benefit) => (
-                  <li key={benefit._id} className="mb-1">
+                {benefit.benefits.map((benefit, idx) => (
+                  <li key={idx} className="mb-1">
                     {benefit}
                   </li>
                 ))}
